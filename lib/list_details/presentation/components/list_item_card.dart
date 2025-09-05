2b060a8b91322420470c965ba711ee449580ac57
Carlos Eduardo <carlos.alves@seconci.rio>
2025-09-05 15:55:06 -0300

    ativar e desativar previsao de preço

--- lib/list_details/presentation/components/list_item_card.dart
+++ lib/list_details/presentation/components/list_item_card.dart
@@ -3,12 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_comprinhas/list_details/domain/entities/list_item.dart';
 import 'package:flutter_comprinhas/list_details/presentation/components/preco_sugerido_chip.dart';
 import 'package:flutter_comprinhas/list_details/presentation/screens/bloc/cart/cart_bloc.dart';
+import 'package:flutter_swipe_action_cell/flutter_swipe_action_cell.dart';
 import 'package:intl/intl.dart';
 
 class ListItemCard extends StatelessWidget {
   final ListItem item;
-  final Animation<double>? animation;
-  final Function(ListItem item)? onDismiss;
+  final VoidCallback? onDelete;
+  final VoidCallback? onEdit;
 
   final bool inCart;
   final String? cartItemId;
@@ -16,127 +17,108 @@ class ListItemCard extends StatelessWidget {
   const ListItemCard({
     super.key,
     required this.item,
-    this.animation,
-    this.onDismiss,
+    this.onDelete,
+    this.onEdit,
     this.inCart = false,
     this.cartItemId,
   });
 
   @override
   Widget build(BuildContext context) {
-    // O conteúdo do Card que será reutilizado
-    final cardContent = ListTile(
-      title: RichText(
-        text: TextSpan(
-          children: <TextSpan>[
-            TextSpan(
-              text: '${item.amount}',
-              style: Theme.of(
-                context,
-              ).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
-            ),
-            TextSpan(
-              text: item.unit.abbreviation,
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Theme.of(context).colorScheme.onSurfaceVariant,
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return SwipeActionCell(
+      key: ObjectKey(item.id),
+      backgroundColor: Colors.transparent,
+      leadingActions: [
+        if (onDelete != null)
+          SwipeAction(
+            icon: Icon(Icons.delete, color: colorScheme.error),
+            color: colorScheme.secondaryContainer,
+            onTap: (_) => onDelete!.call(),
+          ),
+        if (onEdit != null)
+          SwipeAction(
+            icon: Icon(Icons.edit, color: colorScheme.primary),
+            color: colorScheme.secondaryContainer,
+            onTap: (_) => onEdit!.call(),
+          ),
+      ],
+      child: ListTile(
+        title: RichText(
+          text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                text: '${item.amount}',
+                style: Theme.of(
+                  context,
+                ).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                text: item.unit.abbreviation,
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
+              ),
+              TextSpan(
+                text: ' x ',
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                ),
+              ),
+              TextSpan(
+                text: item.name,
+                style: Theme.of(
+                  context,
+                ).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        subtitle: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(24),
+              child: Image.network(
+                item.createdBy.userMetadata?['picture'],
+                height: 24,
+                width: 24,
               ),
             ),
-            TextSpan(
-              text: ' x ',
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Theme.of(context).colorScheme.onSurfaceVariant,
+            const SizedBox(width: 4),
+            const Icon(Icons.calendar_month),
+            Text(DateFormat('dd/MM/yyyy').format(item.createdAt)),
+          ],
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (item.precoSugerido != null)
+              PrecoSugeridoChip(precoSugerido: item.precoSugerido),
+            const SizedBox(width: 4),
+            IconButton(
+              icon: Icon(
+                inCart ? Icons.remove_shopping_cart : Icons.add_shopping_cart,
+                color: inCart ? Colors.redAccent : null,
+                size: 30,
               ),
-            ),
-            TextSpan(
-              text: item.name,
-              style: Theme.of(
-                context,
-              ).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+              onPressed: () {
+                final bloc = context.read<CartBloc>();
+                if (inCart) {
+                  assert(
+                    cartItemId != null,
+                    'cartItemId não pode ser nulo se inCart for true',
+                  );
+                  bloc.add(RemoveFromCart(cartItemId!));
+                } else {
+                  bloc.add(AddToCart(item.id));
+                }
+              },
             ),
           ],
         ),
       ),
-      subtitle: Row(
-        spacing: 2,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(24),
-            child: Image.network(
-              item.createdBy.userMetadata?['picture'],
-              height: 24,
-              width: 24,
-            ),
-          ),
-          const SizedBox(width: 4),
-          Icon(Icons.calendar_month),
-          Text(DateFormat('dd/MM/yyyy').format(item.createdAt)),
-        ],
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        spacing: 4,
-        children: [
-          if (item.precoSugerido != null)
-            PrecoSugeridoChip(precoSugerido: item.precoSugerido),
-          IconButton(
-            icon: Icon(
-              inCart ? Icons.remove_shopping_cart : Icons.add_shopping_cart,
-              color:
-                  inCart ? Colors.redAccent : null, // Cor diferente no carrinho
-              size: 30,
-            ),
-            onPressed: () {
-              final bloc = context.read<CartBloc>();
-              if (inCart) {
-                // Se está no carrinho, deve ter um cartItemId para poder ser removido
-                assert(
-                  cartItemId != null,
-                  'cartItemId não pode ser nulo se inCart for true',
-                );
-                bloc.add(RemoveFromCart(cartItemId!));
-              } else {
-                // Se não está no carrinho, adiciona
-                bloc.add(AddToCart(item.id));
-              }
-            },
-          ),
-        ],
-      ),
     );
-
-    // O widget que será construído
-    Widget card = cardContent;
-
-    // Se a função onDismiss for fornecida, envolvemos com Dismissible
-    if (onDismiss != null) {
-      card = Dismissible(
-        key: ValueKey(item.id),
-        direction: DismissDirection.startToEnd,
-        background: Container(
-          color: Theme.of(context).colorScheme.error,
-          padding: const EdgeInsets.only(left: 16),
-          alignment: Alignment.centerLeft,
-          child: Icon(
-            Icons.delete,
-            color: Theme.of(context).colorScheme.onError,
-          ),
-        ),
-        onDismissed: (direction) {
-          onDismiss!(item);
-        },
-        child: card,
-      );
-    }
-
-    // Se a animação for fornecida, envolvemos com os widgets de transição
-    if (animation != null) {
-      card = SizeTransition(
-        sizeFactor: CurvedAnimation(parent: animation!, curve: Curves.easeOut),
-        child: FadeTransition(opacity: animation!, child: card),
-      );
-    }
-
-    return card;
   }
 }
